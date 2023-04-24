50324ec2398178cf32a2dcdf0e4916870474a4f7
MrAnacin <62877260+MrAnacin@users.noreply.github.com>
2023-04-24 18:49:44 +0500

    Add files via upload

---
+++ javascript/allCards.js
@@ -0,0 +1,258 @@
+const div6_bubi = document.createElement('div');
+const img6_bubi = document.createElement('img');
+img6_bubi.src = '/img/6_bubi.jpg';
+img6_bubi.classList.add('font-face');
+div6_bubi.appendChild(img6_bubi);
+
+const div6_cross = document.createElement('div');
+const img6_cross = document.createElement('img');
+img6_cross.src = '/img/6_cross.jpg';
+img6_cross.classList.add('font-face');
+div6_cross.appendChild(img6_cross);
+
+const div6_peaks = document.createElement('div');
+const img6_peaks = document.createElement('img');
+img6_peaks.src = '/img/6_peaks.jpg';
+img6_peaks.classList.add('font-face');
+div6_peaks.appendChild(img6_peaks);
+
+const div6_worms = document.createElement('div');
+const img6_worms = document.createElement('img');
+img6_worms.src = '/img/6_worms.jpg';
+img6_worms.classList.add('font-face');
+div6_worms.appendChild(img6_worms);
+
+const div7_bubi = document.createElement('div');
+const img7_bubi = document.createElement('img');
+img7_bubi.src = '/img/7_bubi.jpg';
+img7_bubi.classList.add('font-face');
+div7_bubi.appendChild(img7_bubi);
+
+const div7_cross = document.createElement('div');
+const img7_cross = document.createElement('img');
+img7_cross.src = '/img/7_cross.jpg';
+img7_cross.classList.add('font-face');
+div7_cross.appendChild(img7_cross);
+
+const div7_peaks = document.createElement('div');
+const img7_peaks = document.createElement('img');
+img7_peaks.src = '/img/7_peaks.jpg';
+img7_peaks.classList.add('font-face');
+div7_peaks.appendChild(img7_peaks);
+
+const div7_worms = document.createElement('div');
+const img7_worms = document.createElement('img');
+img7_worms.src = '/img/7_worms.jpg';
+img7_worms.classList.add('font-face');
+div7_worms.appendChild(img7_worms);
+
+const div8_bubi = document.createElement('div');
+const img8_bubi = document.createElement('img');
+img8_bubi.src = '/img/8_bubi.jpg';
+img8_bubi.classList.add('font-face');
+div8_bubi.appendChild(img8_bubi);
+
+const div8_cross = document.createElement('div');
+const img8_cross = document.createElement('img');
+img8_cross.src = '/img/8_cross.jpg';
+img8_cross.classList.add('font-face');
+div8_cross.appendChild(img8_cross);
+
+const div8_peaks = document.createElement('div');
+const img8_peaks = document.createElement('img');
+img8_peaks.src = '/img/8_peaks.jpg';
+img8_peaks.classList.add('font-face');
+div8_peaks.appendChild(img8_peaks);
+
+const div8_worms = document.createElement('div');
+const img8_worms = document.createElement('img');
+img8_worms.src = '/img/8_worms.jpg';
+img8_worms.classList.add('font-face');
+div8_worms.appendChild(img8_worms);
+
+const div9_bubi = document.createElement('div');
+const img9_bubi = document.createElement('img');
+img9_bubi.src = '/img/9_bubi.jpg';
+img9_bubi.classList.add('font-face');
+div9_bubi.appendChild(img9_bubi);
+
+const div9_peaks = document.createElement('div');
+const img9_peaks = document.createElement('img');
+img9_peaks.src = '/img/9_peaks.jpg';
+img9_peaks.classList.add('font-face');
+div9_peaks.appendChild(img9_peaks);
+
+const div9_cross = document.createElement('div');
+const img9_bubi = document.createElement('img');
+img9_bubi.src = '/img/9_cross.jpg';
+img9_bubi.classList.add('font-face');
+div9_bubi.appendChild(img9_bubi);
+
+const div9_worms = document.createElement('div');
+const img9_worms = document.createElement('img');
+img9_worms.src = '/img/9_worms.jpg';
+img9_worms.classList.add('font-face');
+div9_worms.appendChild(img9_worms);
+
+const div10_peaks = document.createElement('div');
+const img10_peaks = document.createElement('img');
+img10_peaks.src = '/img/10_peaks.jpg';
+img10_peaks.classList.add('font-face');
+div10_peaks.appendChild(img10_peaks);
+
+const div10_bubi = document.createElement('div');
+const img10_bubi = document.createElement('img');
+img10_bubi.src = '/img/10_bubi.jpg';
+img10_bubi.classList.add('font-face');
+div10_bubi.appendChild(img10_bubi);
+
+const div10_worms = document.createElement('div');
+const img10_worms = document.createElement('img');
+img10_worms.src = '/img/10_worms.jpg';
+img10_worms.classList.add('font-face');
+div10_worms.appendChild(img10_worms);
+
+const div10_cross = document.createElement('div');
+const img10_cross = document.createElement('img');
+img10_cross.src = '/img/10_cross.jpg';
+img10_cross.classList.add('font-face');
+div10_cross.appendChild(img10_cross);
+
+const div_j_peaks = document.createElement('div');
+const img_j_peaks = document.createElement('img');
+img_j_peaks.src = '/img/j_peaks.jpg';
+img_j_peaks.classList.add('font-face');
+div_j_peaks.appendChild(img_j_peaks);
+
+const div_j_bubi = document.createElement('div');
+const img_j_bubi = document.createElement('img');
+img_j_bubi.src = '/img/j_bubi.jpg';
+img_j_bubi.classList.add('font-face');
+div_j_bubi.appendChild(img_j_bubi);
+
+const div_j_worms = document.createElement('div');
+const img_j_worms = document.createElement('img');
+img_j_worms.src = '/img/j_worms.jpg';
+img_j_worms.classList.add('font-face');
+div_j_worms.appendChild(img_j_worms);
+
+const div_j_cross = document.createElement('div');
+const img_j_cross = document.createElement('img');
+img_j_cross.src = '/img/j_cross.jpg';
+img_j_cross.classList.add('font-face');
+div_j_cross.appendChild(img_j_cross);
+
+const div_q_peaks = document.createElement('div');
+const img_q_peaks = document.createElement('img');
+img_q_peaks.src = '/img/q_peaks.jpg';
+img_q_peaks.classList.add('font-face');
+div_q_peaks.appendChild(img_q_peaks);
+
+const div_q_bubi = document.createElement('div');
+const img_q_bubi = document.createElement('img');
+img_q_bubi.src = '/img/q_bubi.jpg';
+img_q_bubi.classList.add('font-face');
+div_q_bubi.appendChild(img_q_bubi);
+
+const div_q_worms = document.createElement('div');
+const img_q_worms = document.createElement('img');
+img_q_worms.src = '/img/q_worms.jpg';
+img_q_worms.classList.add('font-face');
+div_q_worms.appendChild(img_q_worms);
+
+const div_q_cross = document.createElement('div');
+const img_q_cross = document.createElement('img');
+img_q_cross.src = '/img/q_cross.jpg';
+img_q_cross.classList.add('font-face');
+div_q_cross.appendChild(img_q_cross);
+
+const div_k_bubi = document.createElement('div');
+const img_k_bubi = document.createElement('img');
+img_k_bubi.src = '/img/k_bubi.jpg';
+img_k_bubi.classList.add('font-face');
+div_k_bubi.appendChild(img_k_bubi);
+
+const div_k_peaks = document.createElement('div');
+const img_k_peaks = document.createElement('img');
+img_k_peaks.src = '/img/k_peaks.jpg';
+img_k_peaks.classList.add('font-face');
+div_k_peaks.appendChild(img_k_peaks);
+
+const div_k_cross = document.createElement('div');
+const img_k_cross = document.createElement('img');
+img_k_cross.src = '/img/k_cross.jpg';
+img_k_cross.classList.add('font-face');
+div_k_cross.appendChild(img_k_cross);
+
+const div_k_worms = document.createElement('div');
+const img_k_worms = document.createElement('img');
+img_k_worms.src = '/img/k_worms.jpg';
+img_k_worms.classList.add('font-face');
+div_k_worms.appendChild(img_k_worms);
+
+const div_a_bubi = document.createElement('div');
+const img_a_bubi = document.createElement('img');
+img_a_bubi.src = '/img/a_bubi.jpg';
+img_a_bubi.classList.add('font-face');
+div_a_bubi.appendChild(img_a_bubi);
+
+const div_a_peaks = document.createElement('div');
+const img_a_peaks = document.createElement('img');
+img_a_peaks.src = '/img/a_peaks.jpg';
+img_a_peaks.classList.add('font-face');
+div_a_peaks.appendChild(img_a_peaks);
+
+const div_a_worms = document.createElement('div');
+const img_a_worms = document.createElement('img');
+img_a_worms.src = '/img/a_worms.jpg';
+img_a_worms.classList.add('font-face');
+div_a_worms.appendChild(img_a_worms);
+
+const div_a_cross = document.createElement('div');
+const img_a_cross = document.createElement('img');
+img_a_cross.src = '/img/a_cross.jpg';
+img_a_cross.classList.add('font-face');
+div_a_cross.appendChild(img_a_cross);
+
+const containerCards = document.createElement('div');
+containerCards.appendChild(div_a_peaks);
+containerCards.appendChild(div_k_peaks);
+containerCards.appendChild(div_q_peaks);
+containerCards.appendChild(div_j_peaks);
+containerCards.appendChild(div10_peaks);
+containerCards.appendChild(div9_peaks);
+containerCards.appendChild(div8_peaks);
+containerCards.appendChild(div7_peaks);
+containerCards.appendChild(div6_peaks);
+
+containerCards.appendChild(div_a_worms);
+containerCards.appendChild(div_k_worms);
+containerCards.appendChild(div_q_worms);
+containerCards.appendChild(div_j_worms);
+containerCards.appendChild(div10_worms);
+containerCards.appendChild(div9_worms);
+containerCards.appendChild(div8_worms);
+containerCards.appendChild(div7_worms);
+containerCards.appendChild(div6_worms);
+
+containerCards.appendChild(div_a_bubi);
+containerCards.appendChild(div_k_bubi);
+containerCards.appendChild(div_q_bubi);
+containerCards.appendChild(div_j_bubi);
+containerCards.appendChild(div10_bubi);
+containerCards.appendChild(div9_bubi);
+containerCards.appendChild(div8_bubi);
+containerCards.appendChild(div7_bubi);
+containerCards.appendChild(div6_bubi);
+
+containerCards.appendChild(div_a_cross);
+containerCards.appendChild(div_k_cross);
+containerCards.appendChild(div_q_cross);
+containerCards.appendChild(div_j_cross);
+containerCards.appendChild(div10_cross);
+containerCards.appendChild(div9_cross);
+containerCards.appendChild(div8_cross);
+containerCards.appendChild(div7_cross);
+containerCards.appendChild(div6_cross);
+
+containerCards.classList.add('box-container');
