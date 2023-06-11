432ae5c08dc15bb93761a52998aee3dafba273af
MrAnacin <62877260+MrAnacin@users.noreply.github.com>
2023-06-12 00:03:01 +0500

    -

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './css/style.css';
+import { renderBlockChoice, renderScreenChoice } from './mainScreen';
+
+window.application = {
+  blocks: {},
+  screens: {},
+  renderScreen: function (screenName) {
+    this.screens[screenName]();
+  },
+  renderBlock: function (blockName, container) {
+    this.blocks[blockName](container);
+  },
+  levels: [],
+  timers: [],
+  stopInterval: function () {
+    this.timers.forEach((timer) => {
+      clearInterval(timer);
+    });
+  },
+  time: '',
+};
+
+window.application.blocks['blockChoice'] = renderBlockChoice;
+window.application.screens['screenChoice'] = renderScreenChoice;
+window.application.renderScreen('screenChoice');
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    entry: './src/index.js',
+    mode: isProduction ? 'production' : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-react',
+                            ],
+                        },
+                    },
+                    {
+                        loader: 'js-loader',
+                        options: {
+                            transpileOnly: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'static', to: 'static' }],
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    devtool: isProduction ? 'hidden-source-map' : 'source-map',
+}
